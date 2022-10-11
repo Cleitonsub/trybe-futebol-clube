99b555b513ca36841d1620eece54af17d5cc35b4
Cleiton <cleitonrmacedo@gmail.com>
2022-10-11 19:59:06 -0300

    feat: Requisito 14 - Desenvolvido em /app/backend/src/database nas pastas correspondentes, uma migration e um model para a tabela de teams

---
+++ app/backend/src/database/models/TeamsModel.ts
@@ -0,0 +1,40 @@
+import { Model, INTEGER, STRING } from 'sequelize';
+import db from '.';
+import MatchesModel from './MatchesModel';
+// import OtherModel from './OtherModel';
+
+class TeamsModel extends Model {
+  id!: number;
+  teamName!: string;
+}
+
+TeamsModel.init({
+  id: {
+    type: INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
+    allowNull: false,
+  },
+  teamName: {
+    type: STRING,
+    allowNull: false,
+  },
+}, {
+  underscored: true,
+  sequelize: db,
+  modelName: 'teams',
+  timestamps: false,
+});
+
+/*
+  * `Workaround` para aplicar as associations em TS:
+  * Associations 1:N devem ficar em uma das instâncias de modelo
+  */
+
+TeamsModel.belongsTo(MatchesModel, { foreignKey: 'id', as: 'home_team' });
+TeamsModel.belongsTo(MatchesModel, { foreignKey: 'id', as: 'away_team' });
+
+// Example.hasMany(OtherModel, { foreignKey: 'campoC', as: 'campoEstrangeiroC' });
+// Example.hasMany(OtherModel, { foreignKey: 'campoD', as: 'campoEstrangeiroD' });
+
+export default TeamsModel;
