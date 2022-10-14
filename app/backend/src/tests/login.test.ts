b057f1b6216cbcf1dd7a08102eae9d84515dadb0
Cleiton <cleitonrmacedo@gmail.com>
2022-10-14 15:31:50 -0300

    refactor: Alterado nome de constante nos testes de login e retirado comentarios no documento

--- app/backend/src/tests/login.test.ts
+++ app/backend/src/tests/login.test.ts
@@ -13,23 +13,6 @@ chai.use(chaiHttp);
 const { expect } = chai;
 
 describe('Teste em POST /login', () => {
-  /**
-   * Exemplo do uso de stubs com tipos
-   */
-
-  // let chaiHttpResponse: Response;
-
-  // before(async () => {
-  //   sinon
-  //     .stub(Example, "findOne")
-  //     .resolves({
-  //       ...<Seu mock>
-  //     } as Example);
-  // });
-
-  // after(()=>{
-  //   (Example.findOne as sinon.SinonStub).restore();
-  // })
 
   before(async () =>
   sinon.stub(UserModel, 'findOne').resolves(adminData as UserModel)
@@ -37,39 +20,12 @@ describe('Teste em POST /login', () => {
 
   after(() => (UserModel.findOne as sinon.SinonStub).restore());
 
-  it('permite o acesso com dados válidos no front-end', async () => {
-    const result = await chai.request(app).post('/login').send({
+  it('permite o acesso do usuário com dados válidos no front-end', async () => {
+    const chaiHttpResponse = await chai.request(app).post('/login').send({
       email: 'admin@admin.com',
       password: 'secret_admin',
     });
-    expect(result.status).to.be.equal(200);
-    expect(result.body).to.have.property('token');
+    expect(chaiHttpResponse.status).to.be.equal(200);
+    expect(chaiHttpResponse.body).to.have.property('token');
   });
-
-  // it('', async () => {
-    
-  // });
-
-  // it('', async () => {
-    
-  // });
-
-  // it('', async () => {
-    
-  // });
-
-  // it('', async () => {
-    
-  // });
 });
-  // it('...', async () => {
-  //   chaiHttpResponse = await chai
-  //      .request(app)
-  //      ...
-
-  //   expect(...)
-  // });
-
-  // it('Seu sub-teste', () => {
-  //   expect(false).to.be.eq(true);
-  // });
